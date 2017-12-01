7d3d7ffff9d75a98e9432ef34b1e3b5e9c61e38d
wuzhao <liwuzhao97@gmail.com>
2017-12-01 16:38:19 +0800

    create person and category model

---
+++ db/migrate/20171201082829_create_people.rb
@@ -0,0 +1,17 @@
+class CreatePeople < ActiveRecord::Migration[5.1]
+  def change
+    create_table :people do |t|
+      t.string :number, unique: true
+      t.string :name
+      t.string :sex
+      t.integer :age
+      t.string :work_position
+      t.string :title
+      t.string :political_status
+      t.string :education
+      t.datetime :working_time
+      t.datetime :come_university_time
+      t.timestamps
+    end
+  end
+end
