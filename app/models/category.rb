093c7d440f6f7cc99f083e6d1ed5f2f0a1f03c77
wuzhao <liwuzhao97@gmail.com>
2017-12-06 11:39:04 +0800

    validate

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
 
-  validates_presence_of :name
+  validates_presence_of :name, message: "名称不能为空"
+  validates_uniqueness_of :name, message: "该分类已存在"
 
   has_ancestry
 end
